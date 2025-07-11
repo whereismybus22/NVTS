dca0ff761763f17350d151cf5d90cd0649d9917d
21r21a12b7 <21r21a12b7@mlrinstitutions.ac.in>
2025-07-12 00:57:52 +0530

    Update share.js

--- js/share.js
+++ js/share.js
@@ -3,7 +3,8 @@ function shareApp() {
 Now you can track your Institution buses' location 24/7 — from anywhere! 🚍
 🔗 Visit: https://nvts-whereismybus.vercel.app
 👤 Username: NVTS@2004
-🔒 Password: NVTS@2004`;
+🔒 Password: NVTS@2004
+▶️ Quick Tutorial Link : https://youtu.be/sjNcwHucais`;
   const encodedMessage = encodeURIComponent(message);
   const whatsappUrl = `https://wa.me/?text=${encodedMessage}`;
   window.open(whatsappUrl, '_blank');
